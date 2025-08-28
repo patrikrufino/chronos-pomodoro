99518fefa106ba21c0e94f048408284ae4ff2dba
Patrik Rufino <patrik.rufino@gmail.com>
2025-08-28 17:09:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/theme.css'
+import './styles/globals.css'
+import { Heading } from './components/Heading.tsx'
+export function App() {
+  return(
+    <>
+      <Heading />
+    </>    
+  )
+}
---
+++ src/components/Heading.tsx
@@ -0,0 +1,11 @@
+import styles from './Heading.module.css'
+
+interface HeadingProps {
+	children: React.ReactNode
+}
+
+export function Heading(props: HeadingProps) {
+	return(<>
+		<h1 className={styles.heading} >{props.children}</h1>
+	</>)
+}
