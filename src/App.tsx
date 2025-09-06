d75cd96c39ecf70654836d99909c4e8fc5aef57f
Patrik Rufino <patrik.rufino@gmail.com>
2025-09-06 07:11:07 -0300

    Adiciona componentes Menu e CountDown ao App, ajusta estilos de padding e implementa novos estilos para Menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import "./styles/theme.css";
 import "./styles/globals.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 export function App() {
   return (
     <>
       <Container>
         <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
