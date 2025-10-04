f0c6084b7864e76b85a2ce0e3823e2fbe2772df4
Patirk Rufino <patrik.rufino@gmail.com>
2025-10-04 07:06:41 -0300

    ✨ (Feat): Implementa troca entre temas - Implementa a troca entre o tema dark e o tema light ao clicar no botão correspondente do menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,73 @@
-import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from "lucide-react";
+import {
+  HistoryIcon,
+  HomeIcon,
+  SettingsIcon,
+  SunIcon,
+  MoonIcon,
+} from "lucide-react";
 import styles from "./styles.module.css";
+import type { AvaliableThemes } from "../../types/AvaliableThemes";
+import { useState, useEffect } from "react";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvaliableThemes>(() => {
+    const storedTheme =
+      (localStorage.getItem("theme") as AvaliableThemes) || "dark";
+    return storedTheme;
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const actualThemeIcon = {
+    dark: <MoonIcon />,
+    light: <SunIcon />,
+  };
+
+  function handleToggleTheme(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  }
   return (
     <>
-      <nav className={styles.menu}>
-        <a className={styles.menuLink} href="#">
+      <nav className={styles.menu} aria-label="Menu principal">
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Início"
+          title="Início"
+        >
           <HomeIcon />
         </a>
-        <a className={styles.menuLink} href="#">
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Histórico"
+          title="Histórico"
+        >
           <HistoryIcon />
         </a>
-        <a className={styles.menuLink} href="#">
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Configurações"
+          title="Configurações"
+        >
           <SettingsIcon />
         </a>
-        <a className={styles.menuLink} href="#">
-          <SunIcon />
+        <a
+          className={styles.menuLink}
+          href="#"
+          onClick={handleToggleTheme}
+          aria-label="Alternar tema"
+          title="Alternar tema"
+        >
+          {actualThemeIcon[theme]}
         </a>
       </nav>
     </>
---
+++ src/types/AvaliableThemes.ts
@@ -0,0 +1,1 @@
+export type AvaliableThemes = "light" | "dark";
