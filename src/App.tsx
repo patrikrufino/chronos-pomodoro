12136e3c8882211f037645e842dc8675d4b514f6
Patrik Rufino <58515198+patrikrufino@users.noreply.github.com>
2025-11-28 20:02:32 +0000

    feat: integra estado da tarefa no componente App e ajusta tipos no modelo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,25 @@ import { Home } from "./pages/Home";
 
 import "./styles/theme.css";
 import "./styles/globals.css";
+import type { TaskStateModel } from "./models/TaskStateModel";
+import { useState } from "react";
+
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+}
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+  
+  return <Home state={state} setState={setState} />;
 }
